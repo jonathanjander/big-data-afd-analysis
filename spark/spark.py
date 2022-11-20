f6e2c66b2445f88659d3328a1e7dd6130298b9e9
unknown <Thomas.Schaibel@haw-hamburg.de>
2022-11-20 03:03:55 +0100

    impl welt scraper not finished

--- spark/spark.py
+++ spark/spark.py
@@ -1,12 +1,21 @@
-
+import json
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
-from textblob_de import TextBlobDE as textBlob
-#download textblob https://www.nltk.org/data.html
-#newspaper_title_scraper
-from newspaper_title_scraper import scrape_newspaper_title
+# from textblob_de import TextBlobDE as textBlob
+# download textblob https://www.nltk.org/data.html
+# newspaper_title_scraper
+from NewspaperTitleScraper import NewspaperTitleScraper
 
+
+# https://www.welt.de/schlagzeilen/nachrichten-vom-19-11-2022.html
 def main():
+    print("Start main")
+    title_scraper = NewspaperTitleScraper("Welt")
+    title_scraper.start()
+    title_scraper.join()
+    print("Main end")
+
+    """
     spark = SparkSession.builder.getOrCreate()
     spark_context = spark.sparkContext
     titles = scrape_newspaper_title()
@@ -21,6 +30,7 @@ def main():
     tweets = spark.read.csv("../data-warehouse/Afd_tweets.csv", header=True)
     tweets.show()
     tweets.printSchema()
+    """
 
 
 if __name__ == '__main__':
