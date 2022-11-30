43457e024bb8ccf8ef407d2b31a73f7da0658810
Joey <jonathan.jander@gmx.de>
2022-11-30 13:22:13 +0100

    docker for twitter stream (WIP)

--- spark_streaming/read_tweets.py
+++ spark_streaming/read_tweets.py
@@ -1,14 +1,14 @@
-from pyspark.sql import SQLContext, SparkSession
+from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from pyspark.sql import functions as F
 import os
 import sys
 
-from scipy.sparse import spmatrix
-from textblob_de import TextBlobDE
-from datetime import datetime
+#from scipy.sparse import spmatrix
+#from textblob_de import TextBlobDE
 from germansentiment import SentimentModel
+
 
 # TODO
 # encode die tweets vollständig -> nur problem bei parquet files
@@ -26,12 +26,11 @@ def preprocessing(tweets_pre):
     tweets_pre = tweets_pre.na.drop()
     return tweets_pre
 
-
 def sentiment(tweets):
     sentiment_detection_udf = udf(sentiment_detection, ArrayType(StringType()))
+    #sentiment_detection_udf = udf(core_nlp_sentiment_detection, ArrayType(StringType()))
     tweets = tweets.withColumn("sentiment", sentiment_detection_udf('tweet'))
     return tweets
-
 
 def sentiment_detection(text):
     # old way (not good)
@@ -41,6 +40,10 @@ def sentiment_detection(text):
     model = SentimentModel()
     classes, probabilities = model.predict_sentiment([text], output_probabilities=True)
     return [str(classes), str(probabilities)]
+
+
+    # for testing
+    #return ['test', 'test']
 
 
 def tutorial_example_for_testing(lines):
@@ -61,36 +64,41 @@ def create_table(tweets_df, spark):
 
 if __name__ == "__main__":
     # fixing a bug with this
-    os.environ['PYSPARK_PYTHON'] = sys.executable
-    os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+    os.environ['PYSPARK_PYTHON'] = './environment/bin/python'
+    os.environ['PYSPARK_DRIVER_PYTHON'] = './environment/bin/python'
+    #os.environ['PYSPARK_PYTHON'] = 'python3'
+    #os.environ['PYSPARK_DRIVER_PYTHON'] = 'python3'
+    #os.environ['PYSPARK_PYTHON'] = sys.executable
+    #os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
     # create Spark session
-    spark = SparkSession.builder.appName("AfDTweetAnalysis") \
-        .config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector:10.0.5') \
+    spark = SparkSession.builder.appName("afd-tweet-analyzer-stream") \
+        .config('spark.archives', 'pyspark_venv.tar.gz#environment') \
+        .master('spark://localhost:7077') \
         .getOrCreate()
+        #.master('spark://localhost:7077') \
+        #.config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector:10.0.5') \
 
         #.config("spark.mongodb.input.uri", "mongodb://127.0.0.1/afd_tweet_analyzer.tweets") \
         #.config("spark.mongodb.output.uri", "mongodb://127.0.0.1/afd_tweet_analyzer.tweets") \
 
+
+    #spark.sparkContext.addPyFile("german_sentiment.zip")
+    #spark.sparkContext.addPyFile("transformers-4.24.0-py3-none-any.zip")
+
+    print('PYSPARK VERSION: '+spark.version)
+    print('PYTHON VERSION: ' + sys.version)
     # read the tweet data from socket
     lines = spark.readStream.format("socket") \
         .option("host", "127.0.0.1") \
         .option("port", 5555)\
         .load()
-
+    print("is streaming:" + str(lines.isStreaming))
     tweets = preprocessing(lines)
     tweets = sentiment(tweets)
     #tweets.printSchema()
     #create_table(tweets, spark)
     tweets = tweets.repartition(1)
     # encoding still doesn't work
-    """query = tweets.writeStream.queryName("all_tweets") \
-        .format("json") \
-        .option("path", "../data-warehouse/json_files") \
-        .option("checkpointLocation", "../data-warehouse/check") \
-        .option("encoding", 'UTF-8') \
-        .trigger(processingTime='60 seconds') \
-        .start()
-        # .outputMode('append')
     """
     query = tweets.writeStream.queryName("all_tweets") \
         .format("mongodb") \
@@ -101,7 +109,15 @@ if __name__ == "__main__":
         .outputMode("append") \
         .trigger(processingTime='60 seconds') \
         .start()
-
+    """
+    query = tweets.writeStream.queryName("all_tweets") \
+            .format("json") \
+            .option("path", "../data-warehouse/json_files") \
+            .option("checkpointLocation", "../data-warehouse/check") \
+            .option("encoding", 'UTF-8') \
+            .trigger(processingTime='60 seconds') \
+            .start()
+            # .outputMode('append')
 
     """
     # just for testing over the console
--- spark_streaming/twitter-streaming/receive_tweets.py
+++ spark_streaming/twitter-streaming/receive_tweets.py
@@ -2,8 +2,10 @@ import tweepy
 import socket
 # from config import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_SECRET, ACCESS_TOKEN, BEARER
 from datetime import datetime
+from germansentiment import SentimentModel
 
 BEARER = 'AAAAAAAAAAAAAAAAAAAAAOtkigEAAAAAliy%2B7td2nmGkVVCj7orrLwhjJ8w%3DpM4IvekgfVzgAs7NnJoXgLrW1q67jpc2VRjeSmqJCCj8cLpfoc'
+
 
 class TwitterV2Stream(tweepy.StreamingClient):
     def __init__(self, csocket, b_token):
@@ -16,13 +18,15 @@ class TwitterV2Stream(tweepy.StreamingClient):
     def on_tweet(self, tweet):
         try:
             if isinstance(tweet.text, str) and tweet.referenced_tweets == None:
-                #tweet.created_at.strftime("%m/%d/%Y, %H:%M:%S")
+                # tweet.created_at.strftime("%m/%d/%Y, %H:%M:%S")
                 output = str(tweet.text)
                 if tweet.created_at is not None:
-                    output = output + str(";") + str(tweet.created_at) + "\n"
+                    output = output + str(";") + str(tweet.created_at)
                 else:
                     ts = datetime.timestamp(datetime.now())
-                    output = output + str(";") + str(ts) + "\n"
+                    output = output + str(";") + str(ts)
+                sentiment = get_sentiment(tweet.text)
+                output = output + str(';' + sentiment[0] + ';' + sentiment[1] + '\n')
                 print(output)
                 print("-")
                 print("-")
@@ -38,15 +42,23 @@ class TwitterV2Stream(tweepy.StreamingClient):
 def send_tweets_v2(c_socket):
     twitter_client = TwitterV2Stream(csocket=c_socket, b_token=BEARER)
     # "#noafd" -lang:de didnt work aka didnt filter for language
-    twitter_client.add_rules([tweepy.StreamRule('#afd lang:de'), tweepy.StreamRule('#noafd lang:de'), tweepy.StreamRule('afd lang:de')])
-    #twitter_client.delete_rules(['1592908834301001729','1592908834301001730','1592915146938224646','1592919964968681472','1592919964968681473','1592919964968681474'])
+    twitter_client.add_rules(
+        [tweepy.StreamRule('#afd lang:de'), tweepy.StreamRule('#noafd lang:de'), tweepy.StreamRule('afd lang:de')])
+    # twitter_client.delete_rules(['1592908834301001729','1592908834301001730','1592915146938224646','1592919964968681472','1592919964968681473','1592919964968681474'])
     print(twitter_client.get_rules())
     twitter_client.filter(tweet_fields=['referenced_tweets'])
 
 
+def get_sentiment(tweet):
+    model = SentimentModel()
+    classes, probabilities = model.predict_sentiment([tweet], output_probabilities=True)
+    return [str(classes), str(probabilities)]
+
+
 if __name__ == "__main__":
     new_skt = socket.socket()  # initiate a socket object
-    host = "127.0.0.1"  # local machine address
+    # host = "127.0.0.1"  # local machine address
+    host = "0.0.0.0"  # local machine address
     port = 5555  # specific port for your service.
     new_skt.bind((host, port))  # Binding host and port
 
